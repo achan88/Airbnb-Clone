f2dedac5f6d5aa16679bcf40b9f7e526fe3a51c6
AndrewChan28 <144559340+AndrewChan28@users.noreply.github.com>
2024-03-10 19:37:37 -0400

    Button element

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,5 +1,8 @@
 import Container from "../Container";
 import Logo from "./Logo";
+import Search from "./Search";
+import UserMenu from "./UserMenu";
+
 const Navbar = () => {
     return (
         <div className="fixed w-full bg-white z-10 shadow-sm">
@@ -21,7 +24,8 @@ const Navbar = () => {
                         "
                     >
                         <Logo />
-        
+                        <Search />
+                        <UserMenu />
                     </div>
                 </Container>
 
